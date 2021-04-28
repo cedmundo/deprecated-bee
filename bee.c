efb885ce35135830bc80d162cf13e24a2ac089db
Carlos Martínez <ced.mendoza@gmail.com>
2021-04-28 01:21:31 -0500

    Tokenize numbers and strings

--- bee.c
+++ bee.c
@@ -26,6 +26,14 @@ enum bee_token_type {
   BEE_TT_KEYWORD,
 };
 
+enum bee_num_base {
+  BEE_NUM_BASE_NONUM = 0,
+  BEE_NUM_BASE_BIN = 2,
+  BEE_NUM_BASE_OCT = 8,
+  BEE_NUM_BASE_DEC = 10,
+  BEE_NUM_BASE_HEX = 16,
+};
+
 struct bee_token {
   struct bee_highlight *operators;
   struct bee_highlight *keywords;
@@ -38,6 +46,8 @@ struct bee_token {
   int32_t level_stack_top;
   int32_t level_stack_max;
   enum bee_token_type type;
+  enum bee_num_base base;
+  bool is_floating;
 };
 
 struct bee_highlight *bee_highlight_new() {
@@ -90,8 +100,8 @@ struct bee_highlight *bee_builtin_operators() {
 }
 
 struct bee_highlight *bee_builtin_keywords() {
-  const char *defaults[] = {"match",   "with",   "unmatched",
-                            "private", "module", NULL};
+  const char *defaults[] = {"match",  "with",   "unmatched", "handle",
+                            "module", "import", NULL};
   struct bee_highlight root = {.next = NULL, .sequence = NULL};
 
   for (int32_t i = 0; defaults[i] != NULL; i++) {
@@ -126,6 +136,23 @@ struct bee_token bee_starting_token(char *program, int32_t *indent_levels) {
   };
 }
 
+bool isdigitonbase(char c, enum bee_num_base base) {
+  switch (base) {
+  case BEE_NUM_BASE_BIN:
+    return c == '0' || c == '1';
+  case BEE_NUM_BASE_OCT:
+    return isdigit(c) && (c <= '7' || c == '0');
+  case BEE_NUM_BASE_DEC:
+    return isdigit(c);
+  case BEE_NUM_BASE_HEX:
+    return isdigit(c) || (c >= 'A' && c <= 'F');
+  case BEE_NUM_BASE_NONUM:
+    break;
+  }
+
+  return false;
+}
+
 struct bee_token bee_next_token(struct bee_token prev) {
   struct bee_token token = {
       .operators = prev.operators,
@@ -150,12 +177,6 @@ struct bee_token bee_next_token(struct bee_token prev) {
     return token;
   }
 
-  // TODO:
-  //  - Emit EOL when '\n' or ';' is reached and not skipped
-  //  - Don't emit EOL when token between '(' and ')' or '[' and ']'
-  // FIXME:
-  //  - When using INDENT or DEDENT it appears that current row is screwed
-
   // If required, emit an INDENT or a DEDENT instead
   if (start_endl) {
     char *tmp = cur + 1;
@@ -176,22 +197,33 @@ struct bee_token bee_next_token(struct bee_token prev) {
     }
 
     if (line_indent > prev.indent) {
-      token.type = BEE_TT_INDENT;
-
       // Push into level stack (to pop dedents after)
+      token.type = BEE_TT_INDENT;
       token.level_stack[++token.level_stack_top] = line_indent;
       token.indent = line_indent;
+      token.len = 0L;
+      token.row = prev.row;
+      token.col = prev.col;
       return token;
     } else if (line_indent < prev.indent) {
+      // Pop a level from stack
       token.indent = token.level_stack[token.level_stack_top];
       if (line_indent < token.indent && token.level_stack_top > -1) {
         token.type = BEE_TT_DEDENT;
         token.level_stack_top--;
+        token.len = 0L;
+        token.row = prev.row;
+        token.col = prev.col;
         return token;
       }
     }
 
     token.indent = line_indent;
+    // TODO:
+    //  - Emit EOL when '\n' or ';' is reached and not skipped
+    //  - Don't emit EOL when token between '(' and ')' or '[' and ']'
+    // FIXME:
+    //  - When using INDENT or DEDENT it appears that current row is screwed
   }
 
   // Consume heading space characters (tabs, spaces and newlines)
@@ -202,22 +234,127 @@ struct bee_token bee_next_token(struct bee_token prev) {
   }
 
   size_t text_len = 0L;
+  bool is_floating = false;
   enum bee_token_type type = BEE_TT_TEXT;
+  enum bee_num_base base = BEE_NUM_BASE_NONUM;
 
-  // TODO: Try read as a number
-  // TODO: Try read as a string
-  // Try read as an operator
-  struct bee_highlight *cur_op = token.operators;
-  while (cur_op != NULL) {
-    size_t seql = strlen(cur_op->sequence);
-    if (memcmp(cur_op->sequence, cur, seql) == 0) {
-      type = BEE_TT_OPERATOR;
-      text_len += seql;
-      cur += seql;
-      break;
+  // Try to read as a number
+  // FIXME: Make this a function
+  // TODO: (Prefix) Consume prefix operand if no spaces (+0,-0)
+  // TODO: Should throw error if not operator/keyword between text?
+  {
+    // Number: Prefix
+    if (strncmp("0x", cur, 2) == 0) {
+      base = BEE_NUM_BASE_HEX;
+      heading_spaces += 2;
+      cur += 2;
+    } else if (strncmp("0o", cur, 2) == 0) {
+      base = BEE_NUM_BASE_OCT;
+      heading_spaces += 2;
+      cur += 2;
+    } else if (strncmp("0b", cur, 2) == 0) {
+      base = BEE_NUM_BASE_BIN;
+      heading_spaces += 2;
+      cur += 2;
+    } else if (isdigit(*cur)) {
+      base = BEE_NUM_BASE_DEC;
     }
 
-    cur_op = cur_op->next;
+    // Number: Integer part
+    if (base != BEE_NUM_BASE_NONUM) {
+      type = BEE_TT_NUMBER;
+
+      while (NOT_EOF(*cur) && isdigitonbase(*cur, base)) {
+        cur++;
+        text_len++;
+      }
+    }
+
+    // Number: Decimal part
+    if (base == BEE_NUM_BASE_NONUM || base == BEE_NUM_BASE_DEC) {
+      if (NOT_EOF(*cur) && *cur == '.' && isdigit(*(cur + 1))) {
+        type = BEE_TT_NUMBER;
+        base = BEE_NUM_BASE_DEC;
+        is_floating = true;
+        cur++;
+        text_len++;
+
+        while (NOT_EOF(*cur) && isdigitonbase(*cur, base)) {
+          cur++;
+          text_len++;
+        }
+      }
+    }
+
+    // Number: Suffix
+    if (base != BEE_NUM_BASE_NONUM) {
+      if (NOT_EOF(*cur) && *cur == 'e') {
+        char nxt = *(cur + 1);
+        if (NOT_EOF(nxt) && (nxt == '-' || nxt == '+' || isdigit(nxt))) {
+          cur += 2;
+          text_len += 2;
+
+          while (NOT_EOF(*cur) && isdigitonbase(*cur, BEE_NUM_BASE_DEC)) {
+            cur++;
+            text_len++;
+          }
+        }
+      }
+
+      static char *suffixes[] = {"u8",  "u16", "u32", "u64", "i8", "i16",
+                                 "i32", "i64", "f32", "f64", NULL};
+      for (int i = 0; suffixes[i] != NULL; i++) {
+        size_t suffix_len = strlen(suffixes[i]);
+        if (IS_EOF(*cur) || (strlen(cur) < suffix_len)) {
+          continue;
+        }
+
+        if (memcmp(cur, suffixes[i], suffix_len) == 0) {
+          cur += suffix_len;
+          text_len += suffix_len;
+          break;
+        }
+      }
+    }
+  }
+
+  // Try read as a string
+  // FIXME: Make this a function
+  // TODO: Handle bad terminated strings and escape sequences
+  {
+    if (type == BEE_TT_TEXT && *cur == '"') {
+      type = BEE_TT_STRING;
+      text_len++;
+      cur++;
+
+      while (NOT_EOF(*cur) && *cur != '"' && *cur != '\n') {
+        text_len++;
+        cur++;
+      }
+
+      if (*cur == '\"') {
+        text_len++;
+        cur++;
+      } else {
+        // TODO: Error: not a well terminated string
+      }
+    }
+  }
+
+  // Try read as an operator
+  if (type == BEE_TT_TEXT) {
+    struct bee_highlight *cur_op = token.operators;
+    while (cur_op != NULL) {
+      size_t seql = strlen(cur_op->sequence);
+      if (memcmp(cur_op->sequence, cur, seql) == 0) {
+        type = BEE_TT_OPERATOR;
+        text_len += seql;
+        cur += seql;
+        break;
+      }
+
+      cur_op = cur_op->next;
+    }
   }
 
   // Error: this operator is not recognized
@@ -253,14 +390,10 @@ struct bee_token bee_next_token(struct bee_token prev) {
   token.type = type;
   token.len = text_len;
   token.loc = token.loc + heading_spaces;
-  if (start_endl) {
-    token.row = prev.row + 1;
-    token.col = 0L;
-  } else {
-    token.row = prev.row;
-    token.col = prev.col + prev.len + heading_spaces;
-  }
-
+  token.row = prev.row;
+  token.col = prev.col + prev.len + heading_spaces;
+  token.is_floating = is_floating;
+  token.base = base;
   return token;
 }
 
